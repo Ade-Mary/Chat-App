9ea5392b9283f42a6ef398341618dccd1c58a8da
Mary Adewunmi <physayoemma@gmail.com>
2024-01-24 07:25:12 +0100

    Configure Websocket

---
+++ src/main/java/com/example/chatapp/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.chatapp.chat;
+
+import lombok.*;
+import org.apache.logging.log4j.message.Message;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/example/chatapp/config/websocketConfig.java
@@ -0,0 +1,24 @@
+package com.example.chatapp.config;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class websocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
